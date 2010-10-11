a32f655d17cdcc72a27a04905945ccfacc216cb7
joaobispo <joaobispo@localhost>
2010-10-11 18:14:14 +0100

    [MicroBlaze Simulator Tester] - Removed unused imports;

--- experimental/MicroBlazeSimulatorTester/src-base/org/specs/MicroBlazeSimulatorTester/BufferedWriter.java
+++ experimental/MicroBlazeSimulatorTester/src-base/org/specs/MicroBlazeSimulatorTester/BufferedWriter.java
@@ -6,13 +6,7 @@
 package org.specs.MicroBlazeSimulatorTester;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.ancora.SharedLibrary.AppBase.AppUtils;
 import org.ancora.SharedLibrary.AppBase.AppValue;
 import org.ancora.SharedLibrary.IoUtils;
